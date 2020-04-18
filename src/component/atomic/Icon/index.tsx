1811953ed80c29c7b0893d3063abffd0b2a8051e
scd7896 <scd1212@naver.com>
2020-04-19 00:54:27 +0900

    폰트 수정 및 아이콘 내부 설정 완료

--- src/component/atomic/Icon/index.tsx
+++ src/component/atomic/Icon/index.tsx
@@ -8,7 +8,9 @@ interface IconProps {
 const Icon = ({ isRecruit }: IconProps) => {
     return (
         <article className="icon-box-container" style={{backgroundColor: isRecruit? "#ff0000" : "#f89203"}}>
-            
+            {
+                isRecruit? <span className="icon-N-text">N</span> : <span style={{fontSize:'18px', marginLeft: '3px'}}>⏳</span>
+            }
         </article>
     )
 }
