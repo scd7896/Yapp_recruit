58d3c21b32401f2446208b17c3f20e7d248f7a2b
scd7896 <scd1212@naver.com>
2020-04-28 20:39:41 +0900

    홈페이지 아이콘 작성

---
+++ src/component/atomic/YappHomeIcon/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import './styles.scss';
+const YappHomeIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="14.028" height="14" viewBox="0 0 14.028 14">
+      <g id="prefix__icon_home" transform="translate(-264.002 -47)">
+          <g id="prefix__그룹_16" data-name="그룹 16" transform="translate(264.011 46.983)">
+              <path id="prefix__패스_1077" d="M2 6.384l4.144-3.412a1.756 1.756 0 0 1 2.23 0l4.012 3.28" data-name="패스 1077" transform="translate(-.251 -.319)"/>
+              <path id="prefix__사각형_2170" d="M7.788 7.761a1.565 1.565 0 0 1 1.566 1.565v3.53H6.223v-3.53a1.565 1.565 0 0 1 1.565-1.565z" className="prefix__cls-1" data-name="사각형 2170" transform="translate(-.779 -.968)"/>
+              <path id="prefix__패스_140" d="M11.353 5.485v6.475a.682.682 0 0 1-.683.682H4.2a.682.682 0 0 1-.682-.682V5.485" className="prefix__cls-1" data-name="패스 140" transform="translate(-.44 -.683)"/>
+              <path id="prefix__사각형_2172" d="M0 0H14.028V14H0z" data-name="사각형 2172" transform="translate(-.009 .017)" />
+          </g>
+      </g>
+  </svg>
+  )
+}
+
+export default YappHomeIcon;
